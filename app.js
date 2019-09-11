81439463d6a82199fb7f6e165283c5a6ceb6b7ae
Hemant Nirmalkar <hemantnirmalkar17@gmail.com>
2019-09-11 18:01:39 +0530

    basic shema and create function added

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+mongoose.connect('mongodb+srv://hemant123:hemant123@cluster0-wjckl.gcp.mongodb.net/test?retryWrites=true&w=majority')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+})
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+
+// RESTFUL ROUTES
+//landing page
+app.get('/', function (req, res) {
+  res.redirect('/blogs')
+})
+
+app.get('/blogs', function (req, res) {
+  Blog.find({}, function (err, blogs) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('index', { blogs: blogs })
+    }
+  })
+})
+
+//new route
+app.get('/blogs/new', function (req, res) {
+  res.render('new')
+})
+
+//create route
+app.post('/blogs', function (req, res) {
+  Blog.create(req.body.blog, function (err, newBlog) {
+    if (err) {
+      res.render('new')
+    } else {
+      res.redirect('/blogs')
+    }
+  })
+})
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, function () {
+  console.log(`server is listening at port ${PORT}`)
+})
